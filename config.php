74e9573b21ed8ad815f3c9fc4a43a767b7a8b05d
baisiK <ridicc@kellergriller.de>
2016-04-05 13:25:17 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,12 +4,12 @@
     $config['queryport'] = '10011'; //Query port (Default = 10011)
     $config['serverport'] = '9987'; //Instance port, (Default = 9987)
     $config['loginname'] = 'serveradmin'; //Query loginname
-    $config['qpassword'] = '9ahnN0a6'; //Query password        
+    $config['qpassword'] = '******'; //Query password        
     $config['displayname'] = "R4P3_Assigner"; //Bot nickanme
 
     //General settings
     $config['send_enable'] = true; //send a message to client (can be true or false)
     $config['send_method'] = 'text'; //method of message (text/poke)
     $config['send_message'] = '[COLOR=green]You just became a member of the group '; //The client receives that message as soon as he received his group
-    $config['groups'] = array(7, 9, 10);
+    $config['groups'] = array(7, 9, 10); //comma seperated list of groupids
 ?>
