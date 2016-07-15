569da5d39fbbe6cf992f663f349937b9a02f59c9
DrWhat <DrWhat@cryto.net>
2016-07-15 08:18:25 +0100

    Update config.php

--- vBulletinAddon/upload/includes/TeamSpeak3/config.php
+++ vBulletinAddon/upload/includes/TeamSpeak3/config.php
@@ -10,7 +10,7 @@
     //General settings
     $config['send_enable'] = true; //send a message to client (can be true or false)
     $config['send_method'] = 'text'; //method of message (text/poke)
-    $config['send_message_success'] = '[COLOR=green]You just became a member of the group '; //The client receives that message as soon as he received his group
-    $config['send_message_invalid_username'] = "[COLOR=red]You must change your name to your registered username on the forums! ";
+    $config['send_message_success'] = '[b][COLOR=green]You just became a member of the group '; //The client receives that message as soon as he received his group
+    $config['send_message_invalid_username'] = "[b][COLOR=red]You must change your name to your registered username on the forums! ";
     $config['groups'] = array(26); //comma seperated list of groupids
 ?>
